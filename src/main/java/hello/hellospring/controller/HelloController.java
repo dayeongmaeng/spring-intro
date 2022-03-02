3111253f54ba061a3c9cf70d90d6c6cc13b12ca0
dayeongmaeng <dayeongmaeng@gmail.com>
2022-03-02 12:35:29 +0900

    스프링과 빈의 의존 관계 ~ 컴포넌트 스캔과 자동 의존관계 설정

--- src/main/java/hello/hellospring/controller/HelloController.java
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -1,4 +1,4 @@
-package hello.hellospring;
+package hello.hellospring.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
--- src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
+++ src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
@@ -1,6 +1,7 @@
 package hello.hellospring.repository;
 
 import hello.hellospring.domain.Member;
+import org.springframework.stereotype.Repository;
 
 import java.util.*;
 
@@ -8,6 +9,7 @@ import java.util.*;
  * 동시성 문제가 고려되어 있지 않음.
  * 실무에서는 ConcurrentHashMap, AtomicLong 사용 고려
  */
+@Repository
 public class MemoryMemberRepository implements MemberRepository {
 
     private static Map<Long, Member> store = new HashMap<>();
